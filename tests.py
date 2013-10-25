d39ced79b74e6654ba93ef49f1df9e0d14e5b605
Peter Beard <peter.b.beard+github@gmail.com>
2013-10-25 18:11:39 -0400

    Added GPLv2 headers.

--- tests.py
+++ tests.py
@@ -1,3 +1,22 @@
+# Expressionparse v0.1 -- Create syntax trees for mathematical expressions
+#
+# Copyright (C) 2013, Peter Beard <peter.b.beard@gmail.com>
+# 
+# This file is part of Expressionparse.
+# 
+# Expressionparse is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Expressionparse is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Expressionparse. If not, see <http://www.gnu.org/licenses/>.
+
 import expressionparse
 import unittest
 import cmath
